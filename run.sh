c375f652c31cc4b776ebbb5f564a9c627936deb4
Mathuiss <mathijsgeerlings@gmail.com>
2019-11-06 17:28:13 +0100

    We scrapped the whole db in container idea

--- run.sh
+++ run.sh
@@ -1,20 +1,10 @@
-docker rm -f osticket-db
+#! /bin/bash
 docker rm -f osticket
 
-docker run -itd --name osticket-db -e MYSQL_ROOT_PASSWORD=iMIS2008! -e MYSQL_USER=imis_admin -e MYSQL_PASSWORD=imis_admin -e MYSQL_DATABASE=ost_db mysql:5
-docker ps
-
-docker run -itd --name osticket --link osticket-db:mysql -e MYSQL_ROOT_PASSWORD=iMIS2008! -e MYSQL_USER=imis_admin -e MYSQL_PASSWORD=imis_admin -e MYSQL_DATABASE=ost_db -e INSTALL_CONFIG=/data/bin/imis-config.php -p 800:80 -v osticket-upload-volume:/data/upload -v osticket-log-volume:/var/log/nginx osticket-image
+docker run -itd --name osticket -e MYSQL_HOST=172.17.0.1 -e MYSQL_ROOT_PASSWORD=iMIS2008! -e MYSQL_USER=imis_admin -e MYSQL_PASSWORD=imis_admin -e MYSQL_DATABASE=ost_db -e INSTALL_CONFIG=/data/bin/imis-config.php -p 800:80 -v osticket-upload-volume:/data/upload -v osticket-log-volume:/var/log/nginx osticket-image
 docker ps
 
 chown -R ftp:root /var/lib/docker/volumes
-
-sleep 10
-docker ps
-
-docker rm osticket-db
-docker run -itd --name osticket-db -e MYSQL_ROOT_PASSWORD=iMIS2008! -e MYSQL_USER=imis_admin -e MYSQL_PASSWORD=imis_admin -e MYSQL_DATABASE=ost_db mysql:5
-docker ps
-
-echo "[osticket]"
+echo ""
+echo "[osticket container ip]"
 docker exec osticket ifconfig
